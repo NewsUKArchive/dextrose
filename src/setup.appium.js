001ab407be9a50ce0bace150be6aa92d35a9f6b6
Lowry99 <matthewlowry@hotmail.co.uk>
2017-11-20 15:03:53 +0000

    wait for fructose to be displayed

--- src/setup.appium.js
+++ src/setup.appium.js
@@ -9,7 +9,7 @@ let deviceName;
 let app;
 
 
-export default async (config) => {
+export default async(config) => {
     if (!config) {
         throw new Error(`expected appium config but got: ${config}`)
     }
@@ -27,7 +27,7 @@ export default async (config) => {
             platformVersion: config.platformVersion,
             deviceName: config.deviceName,
             app: config.app,
-            automationName:  config.automationName || null
+            automationName: config.automationName || null
         },
         host,
         port
@@ -36,10 +36,20 @@ export default async (config) => {
     global.driver = driver;
     await driver.init(options.desiredCapabilities).setImplicitWaitTimeout(300000);
     global.asserter = wd.asserters;
-    return global.driver.waitForElementsByAccessibilityId(
-       // '//*[@text="Fructose"]',
-       'fructose',
-        global.asserter.isDisplayed,
-        1800000
-    );
+
+    if (config.platformName.toLowerCase() === "ios") {
+        return global.driver.waitForElementsByAccessibilityId(
+            'fructose',
+            global.asserter.isDisplayed,
+            1800000
+        );
+    } else if (config.platformName.toLowerCase() === "android") {
+        return global.driver.waitForElementsByXPath(
+            '//*[@text="Fructose"]',
+            global.asserter.isDisplayed,
+            1800000
+        );
+    } else {
+        throw new Error("Please set platformName in dextrose config")
+    }
 }
