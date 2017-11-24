47fca6b594567e73a36ecc747c74426b06ce38ee
Lowry99 <matthewlowry@hotmail.co.uk>
2017-11-24 18:23:09 +0000

    tests: snap batcher test coverage 100%

--- src/snap-batcher.js
+++ src/snap-batcher.js
@@ -1,40 +1,21 @@
 import log from "./logger";
 
-// export default async(dextrose, snapPath, teardown) => {
-//   try {
-//     const componentsLoaded = await dextrose.client.getLoadedComponents();
+export default async(dextrose, snapPath, teardown) => {
+  try {
+    const componentsLoaded = await dextrose.client.getLoadedComponents();
 
-//     log.info('snapBatcher', `Found Loaded components in the App: ${componentsLoaded}`)
+    log.info('snapBatcher', `Found Loaded components in the App: ${componentsLoaded}`)
 
-//     for (let i = 0; i < componentsLoaded.length; i++) {
-//       await dextrose.client.loadComponent(componentsLoaded[i]);
-//       await dextrose.snapper.snap(`${snapPath}/${componentsLoaded[i]}`)
+    for (let i = 0; i < componentsLoaded.length; i++) {
+      await dextrose.client.loadComponent(componentsLoaded[i]);
+      await dextrose.snapper.snap(`${snapPath}/${componentsLoaded[i]}`)
 
-//       log.info('snapBatcher', `Snapped component: ${componentsLoaded[i]}`)
-//     }
-//   } catch (err) {
+      log.info('snapBatcher', `Snapped component: ${componentsLoaded[i]}`)
+    }
+  } catch (err) {
+    throw (err)
+  } finally {
 
-//     throw new Error(err)
-//   } finally {
-
-//     teardown();
-//   }
-// }
-
-export default (dextrose, snapPath, teardown) =>
-new Promise(async (resolve, reject) => {
-  const componentsLoaded = await dextrose.client.getLoadedComponents()
-  log.info('snapBatcher', `Found Loaded components in the App: ${componentsLoaded}`)
-
-  for (let i = 0; i < componentsLoaded.length; i++) {
-    await dextrose.client.loadComponent(componentsLoaded[i])
-      .then(() => dextrose.snapper.snap(`${snapPath}/${componentsLoaded[i]}`))
-      .catch( async (err) => {
-        await teardown();
-        reject(err);
-      })
+    teardown();
   }
-
-  await teardown();
-  resolve();
-})
+}
--- src/snap-batcher.test.js
+++ src/snap-batcher.test.js
@@ -5,25 +5,41 @@ jest.mock("./web-snapper.js");
 
 const dextroseMock = {
     client: {
-        getLoadedComponents: jest.fn().mockReturnValue(['component1', 'component2']),
+        getLoadedComponents: jest.fn().mockReturnValue(['component1', 'component2', '3']),
         loadComponent: jest.fn()
     },
-
+    snapper: {
+        snap: jest.fn()
+    }
 }
 
-const snapPath =  "path";
+const snapPath = "path";
 
 const teardown = jest.fn();
 
 describe("Snap batcher", () => {
-    it("tears down if error is thrown", () => {
+
+    it("snaps for each loaded component", async() => {
+        await snapBatcher(dextroseMock, snapPath, teardown);
+        expect(dextroseMock.snapper.snap.mock.calls.length).toBe(3)
+    });
+
+    it("tears down after batcher is finished", async() => {
+        await snapBatcher(dextroseMock, snapPath, teardown);
+        expect(teardown.mock.calls.length).toBe(2);
+    });
+
+    it("tears down if error is thrown", async() => {
         dextroseMock.client.loadComponent = () => {
             throw new Error('some error');
         }
+        expect.assertions(2);
 
-        expect(() => snapBatcher(dextroseMock, snapPath, teardown)).toThrow();
-        expect(teardown.mock.calls.length).toBe(1);
+        try {
+            await snapBatcher(dextroseMock, snapPath, teardown)
+        } catch (err) {
+            expect(err.message).toBe("some error")
+            expect(teardown.mock.calls.length).toBe(3);
+        }
     });
-
-    it("calls snapper.snap() for each loaded component", () => {});
 })
