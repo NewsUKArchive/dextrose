641679cd9c5d0bc8dfda8aa17739966cd9eb05bd
Lowry99 <matthewlowry@hotmail.co.uk>
2017-11-27 15:42:15 +0000

    removing unused files

--- src/setup-dextrose.tes.js
+++
@@ -1,8 +0,0 @@
-// import { setupWeb } from "./setup-dextrose";
-
-// describe('dextrose setup', () => {
-//     it("starts a browser", async () => {
-//         jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
-//         await setupWeb();
-//     })
-// })
