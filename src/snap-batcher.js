dc9f4ef3a6091b78a37d781883ba5bd02de4e28c
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-28 14:26:45 +0000

    feat: capability to ignore stories via config

--- src/snap-batcher.js
+++ src/snap-batcher.js
@@ -1,16 +1,21 @@
 import log from "./logger";
 
-export default async(dextrose, snapPath, teardown) => {
+export default async(dextrose, config, teardown) => {
   try {
     const componentsLoaded = await dextrose.client.getLoadedComponents();
+    const ignoredStories = config.ignoredStories;
 
+    const filteredComponents = componentsLoaded.filter(component => ignoredStories.some(ignored => !component.includes(ignored)));
+    
+    console.log()
     log.info('snapBatcher', `Found Loaded components in the App: ${componentsLoaded}`)
+    log.info('snapBatcher', `Will Load Components: ${filteredComponents}`)
+    
+    for (let i = 0; i < filteredComponents.length; i++) {
+      await dextrose.client.loadComponent(filteredComponents[i]);
+      await dextrose.snapper.snap(`${config.snapPath}/${filteredComponents[i]}`)
 
-    for (let i = 0; i < componentsLoaded.length; i++) {
-      await dextrose.client.loadComponent(componentsLoaded[i]);
-      await dextrose.snapper.snap(`${snapPath}/${componentsLoaded[i]}`)
-
-      log.info('snapBatcher', `Snapped component: ${componentsLoaded[i]}`)
+      log.info('snapBatcher', `Snapped component: ${filteredComponents[i]}`)
     }
   } catch (err) {
     throw (err)
--- src/snap-batcher.test.js
+++ src/snap-batcher.test.js
@@ -3,30 +3,36 @@ import snapBatcher from "./snap-batcher";
 jest.mock("./native-snapper.js");
 jest.mock("./web-snapper.js");
 
-const dextroseMock = {
-    client: {
-        getLoadedComponents: jest.fn().mockReturnValue(['component1', 'component2', '3']),
-        loadComponent: jest.fn()
-    },
-    snapper: {
-        snap: jest.fn()
-    }
+let dextroseMock;
+let teardown;
+const config = {
+    snapPath: 'path', 
+    ignoredStories: ['ignorableComponent']
 }
-
-const snapPath = "path";
-
-const teardown = jest.fn();
 
 describe("Snap batcher", () => {
 
+    beforeEach( () => {
+        dextroseMock = {
+            client: {
+                getLoadedComponents: jest.fn().mockReturnValue(['component1', 'component2', '3']),
+                loadComponent: jest.fn()
+            },
+            snapper: {
+                snap: jest.fn()
+            }
+        };
+        teardown = jest.fn();
+    });
+
     it("snaps for each loaded component", async() => {
-        await snapBatcher(dextroseMock, snapPath, teardown);
+        await snapBatcher(dextroseMock, config, teardown);
         expect(dextroseMock.snapper.snap.mock.calls.length).toBe(3)
     });
 
     it("tears down after batcher is finished", async() => {
-        await snapBatcher(dextroseMock, snapPath, teardown);
-        expect(teardown.mock.calls.length).toBe(2);
+        await snapBatcher(dextroseMock, config, teardown);
+        expect(teardown.mock.calls.length).toBe(1);
     });
 
     it("tears down if error is thrown", async() => {
@@ -36,10 +42,17 @@ describe("Snap batcher", () => {
         expect.assertions(2);
 
         try {
-            await snapBatcher(dextroseMock, snapPath, teardown)
+            await snapBatcher(dextroseMock, config, teardown)
         } catch (err) {
             expect(err.message).toBe("some error")
-            expect(teardown.mock.calls.length).toBe(3);
+            expect(teardown.mock.calls.length).toBe(1);
         }
+    });
+
+    it("ignores components defined in config", async () => {
+        dextroseMock.client.getLoadedComponents = jest.fn().mockReturnValue( ["1", "2", "3", "I am a banana", "I am a rat", "I am a"]);
+        config.ignoredStories = ["I am a"];
+        await snapBatcher(dextroseMock, config, teardown);    
+        expect(dextroseMock.snapper.snap.mock.calls.length).toBe(3);
     });
 })
