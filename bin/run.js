470f9f1167a7480bd2d6fd00d958fe3824ae2f92
Lowry99 <matthewlowry@hotmail.co.uk>
2017-11-30 19:07:41 +0000

    feature: able to pass log level in through cli

--- bin/run.js
+++ bin/run.js
@@ -2,10 +2,12 @@
 const program = require("commander");
 const dextrose = require("../index").default;
 const resolve = require('path').resolve
+const setupLogger = require('../lib/logger').setupLogger
 
 program
   .version("0.0.1")
   .option("-c, --config [dextrose-config]", "please specify the dextrose config")
+  .option("-l, --loglevel [log-level]")
   .parse(process.argv);
 
 
@@ -13,6 +15,10 @@ if (!program.config)
   throw new Error(
     "Please specify the dextrose config: --config [directory/config.js]"
   );
+
+  process.env.LOGLEVEL = program.loglevel ? program.loglevel : '';
+  setupLogger();
+
   const path = resolve(program.config);
   const config = require(path);
   dextrose(config);
