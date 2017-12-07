258a8580018c05ec9112232d8e49fad8ba470339
Lowry99 <matthewlowry@hotmail.co.uk>
2017-12-07 13:27:35 +0000

    web snap tests passing

--- src/web-snapper.js
+++ src/web-snapper.js
@@ -8,31 +8,21 @@ module.exports = class WebSnapper {
     }
 
     async snap(outputPath) {
-
-       
-
         for (let i = 0; i < this.breakpoints.length; i++) {
             try {
                 if (typeof outputPath !== 'string') {
                     throw Error(`Output path should be a string recieved: ${outputPath}`);
                 }
-                this.browser.setWindowSize(this.breakpoints[i], 1200, async(err) => {
-                    if (err) throw (err);
+                await this.browser.setWindowSize(this.breakpoints[i], 1200)
 
-                    log.verbose('web-snapper', `set browser window to: ${this.breakpoints[i]}`)
+                log.verbose('web-snapper', `set browser window to: ${this.breakpoints[i]}`)
 
-                    const outputPathWithExtension = `${outputPath}.${this.platform}.width-${this.breakpoints[i]}.png`
-                    log.verbose('web-snapper', `taking snapshot at path: ${outputPathWithExtension}`)
-                    const screenshot = await this.browser.takeScreenshot();
-                    console.log('------------------------------------');
-                    console.log(screenshot);
-                    console.log('------------------------------------');
+                const outputPathWithExtension = `${outputPath}.${this.platform}.width-${this.breakpoints[i]}.png`
+                log.verbose('web-snapper', `taking snapshot at path: ${outputPathWithExtension}`)
+                this.browser.takeScreenshot((err, screenshot) => {
                     fs.writeFileSync(outputPathWithExtension, screenshot, 'base64');
-                });
+                })
             } catch (err) {
-                console.log('------------------------------------');
-                console.log("error found " + err);
-                console.log('------------------------------------');
                 throw err;
             }
         };
