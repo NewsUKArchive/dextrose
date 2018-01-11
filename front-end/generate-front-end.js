c4532813e855f7d3bee6c29fca6f4c558b147f73
Rohan Janjua <rohan.janjua@gmail.com>
2018-01-11 18:30:16 +0000

    chore: prettier

--- front-end/generate-front-end.js
+++ front-end/generate-front-end.js
@@ -1,60 +1,63 @@
-const fs = require('fs');
-const path = require('path');
-const AWS = require('aws-sdk');
-const pug = require('pug');
+const fs = require("fs");
+const path = require("path");
+const AWS = require("aws-sdk");
+const pug = require("pug");
 
 module.exports = (bucket, commitHash, opts) => {
-    AWS.config.update({region: opts.region});
-    var s3 = new AWS.S3();
-    s3.listObjects({Bucket: bucket, Prefix: commitHash}, function(err, data){
-        const contents = data.Contents;
-        const files = contents.map(obj => {
-            return `${s3.endpoint.href}${bucket}/${obj.Key}`;
-        })
-        // Assumes dextrose output convention
-        const web_photos = files.filter(file => file.includes('.web.png'));
-        const ios_photos = files.filter(file => file.includes('.ios.png'));
-        const android_photos = files.filter(file => file.includes('.android.png'));
-        const names = Array.from(new Set(files.map(f => f.replace(/(.ios|.web|.android).png/,""))));
-        const templatePath = path.join(__dirname, 'template.pug');
-        let compiledFunction = pug.compileFile(templatePath);
-        
-        const thing = compiledFunction({
-            names: Array.from(names),
-            ios_pics: ios_photos,
-            android_pics: android_photos,
-            web_pics: web_photos
-        });
+  AWS.config.update({ region: opts.region });
+  var s3 = new AWS.S3();
+  s3.listObjects({ Bucket: bucket, Prefix: commitHash }, function(err, data) {
+    const contents = data.Contents;
+    const files = contents.map(obj => {
+      return `${s3.endpoint.href}${bucket}/${obj.Key}`;
+    });
+    // Assumes dextrose output convention
+    const web_photos = files.filter(file => file.includes(".web.png"));
+    const ios_photos = files.filter(file => file.includes(".ios.png"));
+    const android_photos = files.filter(file => file.includes(".android.png"));
+    const names = Array.from(
+      new Set(files.map(f => f.replace(/(.ios|.web|.android).png/, "")))
+    );
+    const templatePath = path.join(__dirname, "template.pug");
+    let compiledFunction = pug.compileFile(templatePath);
 
-        fs.writeFile("index.html", thing, function(err) {
-            if(err) {
-                return console.log(err);
-            }
+    const thing = compiledFunction({
+      names: Array.from(names),
+      ios_pics: ios_photos,
+      android_pics: android_photos,
+      web_pics: web_photos
+    });
 
-            const pagePath = path.join(__dirname, 'index.html');
-        
-            var fileStream = fs.createReadStream(pagePath);
+    fs.writeFile("index.html", thing, function(err) {
+      if (err) {
+        logger.error(err);
+        return;
+      }
 
-            fileStream.on('error', function(err) {
-            console.log('File Error', err);
-            });
+      const pagePath = path.join(__dirname, "index.html");
 
-            var uploadParams = {
-                Bucket: bucket, 
-                Key: `${commitHash}/index.html`, 
-                Body: fileStream,
-                ContentType: "text/html"
-            };
+      var fileStream = fs.createReadStream(pagePath);
 
-            // call S3 to retrieve upload file to specified bucket
-            s3.putObject (uploadParams, function (err, data) {
-                if (err) {
-                    console.log("Error", err);
-                } 
-                if (data) {
-                    console.log("Uploaded index.html successfully");
-                }
-            });
-        }); 
-    })
-}
+      fileStream.on("error", function(err) {
+        logger.error("File Error", err);
+      });
+
+      var uploadParams = {
+        Bucket: bucket,
+        Key: `${commitHash}/index.html`,
+        Body: fileStream,
+        ContentType: "text/html"
+      };
+
+      // call S3 to retrieve upload file to specified bucket
+      s3.putObject(uploadParams, function(err, data) {
+        if (err) {
+          logger.error("Error", err);
+        }
+        if (data) {
+          logger.info("Uploaded index.html successfully");
+        }
+      });
+    });
+  });
+};
--- front-end/upload_snaps.js
+++ front-end/upload_snaps.js
@@ -1,33 +1,36 @@
-var fs = require('fs');
-var AWS = require('aws-sdk');
-var path = require('path');
+var fs = require("fs");
+var AWS = require("aws-sdk");
+var path = require("path");
 
 module.exports = (bucket, commitHash, snapPath, opts) => {
-  AWS.config.update({region: opts.region});
+  AWS.config.update({ region: opts.region });
   const s3 = new AWS.S3();
-  const files = fs.readdirSync(snapPath).map(f => `${path.join(__dirname, snapPath)}/${f}`);
+  const files = fs
+    .readdirSync(snapPath)
+    .map(f => `${path.join(__dirname, snapPath)}/${f}`);
 
   for (file in files) {
-      const fileStream = fs.createReadStream(files[file]);
+    const fileStream = fs.createReadStream(files[file]);
 
-      fileStream.on('error', function(err) {
-        console.log('File Error', err);
-      });
+    fileStream.on("error", function(err) {
+      logger.error("File Error", err);
+    });
 
-      const uploadParams = {
-        Bucket: bucket, 
-        Key: `${commitHash}/${path.basename(files[file])}`, 
-        Body: fileStream, 
-        ContentType: 'image/png'
-      };
+    const uploadParams = {
+      Bucket: bucket,
+      Key: `${commitHash}/${path.basename(files[file])}`,
+      Body: fileStream,
+      ContentType: "image/png"
+    };
 
-      // call S3 to retrieve upload file to specified bucket
-      s3.putObject (uploadParams, function (err, data) {
-        if (err) {
-          console.log("Error", err);
-        } if (data) {
-          console.log("Upload Success", data.Location);
-        }
-      });
+    // call S3 to retrieve upload file to specified bucket
+    s3.putObject(uploadParams, function(err, data) {
+      if (err) {
+        logger.error("Error", err);
+      }
+      if (data) {
+        logger.info("Upload Success", data.Location);
+      }
+    });
   }
-}
+};
