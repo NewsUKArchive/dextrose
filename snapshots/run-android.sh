ab50502cd71dca183f1351d6d62a6927530cec1f
Lowry99 <matthewlowry@hotmail.co.uk>
2018-01-23 14:52:19 +0000

    adding uiAutomator2

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,12 +1,15 @@
-# appium &
-# APPIUM_PID=$!
+appium &
+APPIUM_PID=$!
 emulator @Nexus_5X_API_25 -no-boot-anim &
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
 adb reverse tcp:7811 tcp:7811
 adb reverse tcp:4723 tcp:4723
+npx react-native start --root $(pwd) --projectRoots $(pwd) &
+PACKAGER_PID=$!
 
 react-native run-android --no-packager
-LOGLEVEL=verbose node run-dextrose.android.js
+npx dextrose run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
 
-dextrose --config ./fructose/dextrose.android.js  --loglevel verbose
+kill -9 $PACKAGER_PID
+kill -9 $APPIUM_PID
--- snapshots/run-ios.sh
+++ snapshots/run-ios.sh
@@ -1,11 +1,10 @@
-
 appium &
 APPIUM_PID=$!
 xcrun simctl boot 'iPhone 7'
 
 npx react-native start --root $(pwd) --projectRoots $(pwd) &
-react-native run-ios --no-packager
 PACKAGER_PID=$!
+react-native run-ios --no-packager
 npx dextrose run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
 
 kill -9 $PACKAGER_PID
