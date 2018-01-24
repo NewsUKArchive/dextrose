4148cbde056822119978c83179d796148d9a357b
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-24 11:52:58 +0000

    changing emuator version in run-android

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,7 +1,7 @@
 appium &
 APPIUM_PID=$!
 
-emulator @Nexus_5X_API_22 -no-boot-anim &
+emulator @Nexus_5X_API_25 -no-boot-anim &
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
 adb reverse tcp:7811 tcp:7811
