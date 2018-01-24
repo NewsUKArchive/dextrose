185c436ba3e00377c82d8bac8e3285ebef7d8ec6
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-24 11:34:55 +0000

    using release vairant to deal with pacakger connection issue

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,16 +1,16 @@
 appium &
 APPIUM_PID=$!
 
-emulator @Nexus_5X_API_25 -no-boot-anim &
+/Users/kourosaliabadi/Library/Android/sdk/tools/emulator @Nexus_5X_API_22 -no-boot-anim &
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
 adb reverse tcp:7811 tcp:7811
 adb reverse tcp:4723 tcp:4723
 
-npx react-native start --root $(pwd) --projectRoots $(pwd) --reset-cache &
-PACKAGER_PID=$!
+# npx react-native start --root $(pwd) --projectRoots $(pwd) --reset-cache &
+# PACKAGER_PID=$!
 
-react-native run-android --no-packager
+react-native run-android --variant=release
 npx dextrose run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
 
 kill -9 $PACKAGER_PID
