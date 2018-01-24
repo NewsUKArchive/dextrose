f282621e9921f9ffd90adfaab6b2a24695e221b9
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-24 11:52:20 +0000

    removing unwanted local path

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,17 +1,13 @@
 appium &
 APPIUM_PID=$!
 
-/Users/kourosaliabadi/Library/Android/sdk/tools/emulator @Nexus_5X_API_22 -no-boot-anim &
+emulator @Nexus_5X_API_22 -no-boot-anim &
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
 adb reverse tcp:7811 tcp:7811
 adb reverse tcp:4723 tcp:4723
 
-# npx react-native start --root $(pwd) --projectRoots $(pwd) --reset-cache &
-# PACKAGER_PID=$!
-
 react-native run-android --variant=release
 npx dextrose run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
 
-kill -9 $PACKAGER_PID
 kill -9 $APPIUM_PID
