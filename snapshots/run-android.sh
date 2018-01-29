f1c64eb79ad7134584994829354c74b8b9672fbc
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-29 15:56:13 +0000

    cleaning scripts

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,6 +1,3 @@
-appium &
-APPIUM_PID=$!
-
 emulator @Nexus_5X_API_25 -no-boot-anim &
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
@@ -9,5 +6,3 @@ adb reverse tcp:4723 tcp:4723
 
 react-native run-android --variant=release
 npx dextrose run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
-
-kill -9 $APPIUM_PID
