cc00ad4aef8cf83289daa6511630688ca79ed9b9
kaliabadi <kouros.aliabadi@hotmail.com>
2018-01-29 15:31:50 +0000

    removing client config as causing connection issues and unnecessary

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,7 @@
 const SocketClient = require("socket.io-client");
 const DextroseClient = require("./client");
 
-const config = {
-  transports: ["websocket"],
-  query: {
-    clientType: "snapper"
-  }
-};
-
-const sc = port => new SocketClient(`http://localhost:${port || 7811}`, config);
+const sc = port => new SocketClient(`http://localhost:${port || 7811}`);
 const dextroseClient = port => new DextroseClient(sc(port));
 
 export default dextroseClient;
