167d7614e932dccef2dd3adde475d6e757bf0d4c
Rohan Janjua <rohan.janjua@gmail.com>
2018-02-06 18:41:26 +0000

    chore: uncomment ios build

--- snapshots/run-ios.sh
+++ snapshots/run-ios.sh
@@ -5,7 +5,7 @@ rm snaps/goodbyeWorld.ios.png || echo "goodbyeWorld.ios.png does not exist"
 
 ./node_modules/.bin/react-native start --root $(pwd) --projectRoots $(pwd) --config $(pwd)/rn-cli.config.js --reset-cache &
 PACKAGER_PID=$!
-#./node_modules/.bin/react-native run-ios --no-packager
+./node_modules/.bin/react-native run-ios --no-packager
 ./node_modules/.bin/dextrose run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
 
 kill -9 $PACKAGER_PID
