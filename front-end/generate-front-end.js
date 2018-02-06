961929e4ece03351f0fe63807ecc23acf8e6d556
Rohan Janjua <rohan.janjua@gmail.com>
2018-02-06 10:38:43 +0000

    fix: ensure the ui puts the correct images with the correct labels across all platforms (#53)
    * fix: ensure the ui puts the correct images with the correct labels across all platforms
    
    * chore: a bit of refactoring
    
    * chore: remove unneeded whitespace

--- front-end/generate-front-end.js
+++ front-end/generate-front-end.js
@@ -7,26 +7,37 @@ const log = require("../lib/logger").default;
 module.exports = (bucket, commitHash, opts) => {
   AWS.config.update({ region: opts.region });
   const s3 = new AWS.S3();
+
   s3.listObjects({ Bucket: bucket, Prefix: commitHash }, (err, data) => {
-    const contents = data.Contents;
-    const s3ImagesDetails = contents.map(details => {
-      return `${s3.endpoint.href}${bucket}/${details.Key}`;
-    });
-    // Assumes dextrose output convention
-    const webShots = s3ImagesDetails.filter(file => file.includes(".web.png"));
-    const iosShots = s3ImagesDetails.filter(file => file.includes(".ios.png"));
-    const androidShots = s3ImagesDetails.filter(file => file.includes(".android.png"));
-    const shotNames = Array.from(
-      new Set(s3ImagesDetails.map(f => f.replace(/(.ios|.web|.android).png/, "")))
-    );
+    const names = []
+    const shots = data.Contents.reduce( (collection, details) => {
+      const url = `${s3.endpoint.href}${bucket}/${details.Key}`;
+      
+      if (!url.includes(".png")) {
+        return collection;
+      }
+
+      const storyDetails = url.match(/dextrose-test\/(.*)\.(.*)\.png/);
+      const storyName = storyDetails[1];
+      const platform = storyDetails[2];
+
+      if (!collection[storyName]) {
+        names.push(storyName);
+        collection[storyName] = {};
+      }
+
+      collection[storyName][platform] = url;
+
+      return collection;
+    } ,{});
+
     const templatePath = path.join(__dirname, "template.pug");
     const compileTemplate = pug.compileFile(templatePath);
 
+
     const dextrosePresentation = compileTemplate({
-      names: shotNames,
-      ios_pics: iosShots,
-      android_pics: androidShots,
-      web_pics: webShots
+      names: names,
+      shots: shots
     });
 
     const pagePath = path.join(__dirname, "index.html");
