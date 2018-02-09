30db695aa7189725de4397cb299d680e23c31930
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-09 13:58:45 +0000

    adding clean to package.json

--- bin/generate-stories.js
+++ bin/generate-stories.js
@@ -2,34 +2,34 @@ const fs = require('fs');
 
 const dextroseExtension = '.dextrose.tmp.js';
 
-const overRideStories = (files, stringToFind, stringToReplace) => new Promise((resolve) => {
-  files.forEach((file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-      if (data.includes(stringToFind)) {
-        const updatedData = data.replace(stringToFind, stringToReplace);
-        const trimmedFile = file.replace('.js', '');
-        const newFilePath = trimmedFile.concat(dextroseExtension);
-
-        fs.writeFile(newFilePath, updatedData);
-      }
+const overRideStories = (dirPath, files, stringToFind, stringToReplace) =>
+  new Promise((resolve, reject) => {
+    files.forEach((file) => {
+      fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        if (data.includes(stringToFind)) {
+          const updatedData = data.replace(stringToFind, stringToReplace);
+          const trimmedFile = file.replace('.js', '');
+          const newFilePath = trimmedFile.concat(dextroseExtension);
+          fs.writeFileSync(`${dirPath}/${newFilePath}`, updatedData);
+        }
+      });
     });
+    resolve();
   });
-  resolve();
-});
 
-const findStoryFiles = matchers => new Promise((resolve, reject) => {
-  fs.readdir(__dirname, 'utf8', (err, files) => {
+const findStoryFiles = (dir, matchers) => new Promise((resolve, reject) => {
+  fs.readdir(dir, 'utf8', (err, files) => {
     if (err) reject(err);
 
     if (!files) {
       reject(Error('No files found'));
     } else {
       const storyFiles = files.map((file) => {
-        const isStoryfile = matchers.some(matcher => file.includes(matcher));
-        if (isStoryfile) {
+        const isStoryFile = matchers.some(matcher => file.includes(matcher));
+        if (isStoryFile) {
           return file;
         }
 
@@ -37,31 +37,30 @@ const findStoryFiles = matchers => new Promise((resolve, reject) => {
       });
 
       const cleanedStoryFiles = storyFiles.filter(story => story !== undefined);
-
       resolve(cleanedStoryFiles);
     }
   });
 });
 
-const generateStories = () => new Promise((resolve) => {
+const generateStories = dir => new Promise((resolve, reject) => {
   const storyExtensionFilesToFind = ['.stories.js', 'stories.web', 'stories.native'];
 
-  findStoryFiles(storyExtensionFilesToFind)
+  findStoryFiles(dir, storyExtensionFilesToFind)
     .then((storyFiles) => {
       const stringToFind = '@storybook/react-native';
       const stringToReplace = 'dextrose/storiesOfOverloader';
-      overRideStories(storyFiles, stringToFind, stringToReplace);
+      overRideStories(dir, storyFiles, stringToFind, stringToReplace);
       resolve();
     })
-    .catch(err => Error(err));
+    .catch(err => reject(err));
 });
 
 
-const cleanDextroseStories = () => {
-  findStoryFiles([dextroseExtension])
+const cleanDextroseStories = (dir) => {
+  findStoryFiles(dir, [dextroseExtension])
     .then((files) => {
       files.forEach((file) => {
-        fs.unlink(`./${file}`, (err) => {
+        fs.unlink(`${dir}/${file}`, (err) => {
           if (err) {
             throw err;
           }
