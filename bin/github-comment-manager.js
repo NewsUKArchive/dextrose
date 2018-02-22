d488458fabdc4c66e5bb9a3812f32de0b5713804
kaliabadi <kouros.aliabadi@hotmail.com>
2018-02-22 13:39:24 +0000

    adding tests

---
+++ bin/github-comment-manager.js
@@ -0,0 +1,70 @@
+const request = require('request');
+
+const existingComments = (accountName, accountKey, issueNumber) =>
+  new Promise((resolve, reject) => {
+    const auth = 'Basic ' + new Buffer(accountName + ':' + accountKey).toString('base64');
+    const options = {
+      url: `https://api.github.com/repos/newsuk/times-components/issues/${issueNumber}/comments`,
+      headers: {
+       Authorization: auth,
+       'User-Agent': accountName
+      }
+    };
+
+    request.get(options, (error, response, body) => {
+      if (error) reject(error);
+      const ids = JSON.parse(body)
+        .filter(({ user }) => user.login === accountName)
+        .map(({ id }) => id);
+      
+      resolve(ids);
+    });
+});
+
+const deleteComments = (commentsToDelete, accountName, accountKey) =>
+  new Promise((resolve, reject) => {
+    commentsToDelete.forEach((commentId) => {
+      const auth = 'Basic ' + new Buffer(accountName + ':' + accountKey).toString('base64');
+      const deleteOptions = {
+        url: `https://api.github.com/repos/newsuk/times-components/issues/comments/${commentId}`,
+        headers: {
+          Authorization: auth,
+          'User-Agent': accountName
+        }
+      };
+
+      request.delete(deleteOptions, (error) => {
+        if (error) reject(error);
+      });
+    });
+    resolve('deleted');
+  });
+
+const postComment = (accountName, accountKey, documentPath, issueNumber) =>
+  new Promise((resolve, reject) => {
+    const auth = 'Basic ' + new Buffer(accountName + ':' + accountKey).toString('base64');
+    const postCommentOptions = {
+      url: `https://api.github.com/repos/newsuk/times-components/issues/${issueNumber}/comments`,
+      headers: {
+        'Authorization': auth,
+        'User-Agent': accountName
+      },
+      body: `{ "body": "Please find visual snapshots of your changed components here: ${documentPath} "}`
+    };
+
+    request.post(postCommentOptions, (error) => {
+      if (error) reject(error);
+      resolve('commented');
+    });
+});
+
+const publishStories = async (accountName, accountKey, documentPath, issueNumber) => {
+  const commentsToDelete = await existingComments(accountName, accountKey, issueNumber);
+  await deleteComments(commentsToDelete, accountName, accountKey);
+  await postComment(accountName, accountKey, documentPath, issueNumber);
+}
+
+module.exports = {
+  publishStories,
+  existingComments
+};
---
+++ bin/github-comment-manager.test.js
@@ -0,0 +1,86 @@
+/* global describe it expect */
+const nock = require('nock');
+const githubCommentManager = require('./github-comment-manager');
+
+const accountName = 'testAccountName';
+const key = 'testKey';
+
+const noCommentsResponse = [
+  {
+    id: 1,
+    user: {
+      login: 'not test account name'
+    }
+  }
+];
+
+const mixedCommentsResponse = [
+  {
+    id: 1,
+    user: {
+      login: 'not test account name'
+    }
+  },
+  {
+    id: 2,
+    user: {
+      login: 'testAccountName'
+    }
+  },
+  {
+    id: 3,
+    user: {
+      login: 'a different non test account name'
+    }
+  }
+];
+
+const multipleUserCommentsResponse = [
+  {
+    id: 1,
+    user: {
+      login: 'testAccountName'
+    }
+  },
+  {
+    id: 2,
+    user: {
+      login: 'testAccountName'
+    }
+  }
+];
+
+describe('publish stories to github pull request', () => {
+  it('should not pick up other comments', async () => {
+    if (!nock.isActive()) { nock.activate(); }
+    nock('https://api.github.com')
+      .get('/repos/newsuk/times-components/issues/1/comments')
+      .reply(200, noCommentsResponse);
+    
+    const commentsToDelete = await githubCommentManager.existingComments(accountName, key, 1);
+
+    expect(commentsToDelete).toEqual([]);
+  });
+
+  it('should only pick comments from the specified account where there are many', async () => {
+    if (!nock.isActive()) { nock.activate(); }
+    nock('https://api.github.com')
+      .get('/repos/newsuk/times-components/issues/1/comments')
+      .reply(200, mixedCommentsResponse);
+    
+    const commentsToDelete = await githubCommentManager.existingComments(accountName, key, 1);
+
+    expect(commentsToDelete).toEqual([2]);
+  });
+
+  it('should pick up all comments from specified user', async () => {
+    if (!nock.isActive()) { nock.activate(); }
+    nock('https://api.github.com')
+      .get('/repos/newsuk/times-components/issues/1/comments')
+      .reply(200, multipleUserCommentsResponse);
+    
+    const commentsToDelete = await githubCommentManager.existingComments(accountName, key, 1);
+
+    expect(commentsToDelete).toEqual([1, 2]);
+  });
+});
