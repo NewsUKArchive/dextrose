cb4187c4108dfaa3c4adc8c15eef2c37233cfd7f
kaliabadi <kouros_aliabadi@hotmail.com>
2018-04-27 17:21:40 +0100

    fix: adding the android tests

--- snapshots/run-android.sh
+++ snapshots/run-android.sh
@@ -1,9 +1,9 @@
-emulator @Nexus_5X_API_25 -no-boot-anim &
+/Users/kourosaliabadi/Library/Android/sdk/tools/emulator @Nexus_5X_API_25 -no-boot-anim &
 
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
 adb reverse tcp:7811 tcp:7811
 adb reverse tcp:4723 tcp:4723
 
-./node_modules/.bin/react-native run-android --variant=release --config $(pwd)/rn-cli.config.js
-./node_modules/.bin/dextrose run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
+./node_modules/.bin/react-native run-android --variant=release --no-packager
+node "../bin/run" run --config ./fructose/dextrose.android.js --snapshotWait 2000 --loglevel verbose
---
+++ snapshots/tests/e2e.android.tests.js
@@ -0,0 +1,22 @@
+const test = require('ava');
+const { execFileSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const fileExists = imagePath => fs.existsSync(path.join(__dirname, imagePath));
+
+test.before(t => {
+  execFileSync(path.join(__dirname, '../run-android.sh'), [], { stdio: 'inherit' });
+});
+
+test.only('dextrose generates a snapshello.android.png', t => {
+  t.true(fileExists('../snaps/snapshello.android.png'));
+});
+
+test('dextrose generates a snapsgoodbye.android.png', t => {
+  t.true(fileExists('../snaps/snapsgoodbye.android.png'));
+});
+
+test('dextrose does not generate a snapsignore.android.png', t => {
+  t.false(fileExists('../snaps/snapsignore.android.png'));
+});
