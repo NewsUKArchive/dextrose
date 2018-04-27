e7dde53c49007d056faf2d6643bfef94f973ad14
Kouros Aliabadi <kaliabadi@users.noreply.github.com>
2018-04-27 16:02:37 +0100

    Refactor e2e (#75)
    * refactor: ios
    
    * refactor: ios
    
    * wtfuck
    
    * chore: removing unwanted code
    
    * fix: incorrect assertions in e2e tests

--- snapshots/run-ios.sh
+++ snapshots/run-ios.sh
@@ -3,9 +3,9 @@ xcrun simctl boot 'iPhone 7'
 rm snaps/hello_worlds.ios.png || echo "hello_worlds.ios.png does not exist"
 rm snaps/goodbyeWorld.ios.png || echo "goodbyeWorld.ios.png does not exist"
 
-./node_modules/.bin/react-native start --root $(pwd) --projectRoots $(pwd) --config $(pwd)/rn-cli.config.js --reset-cache &
+./node_modules/.bin/react-native start --root $(pwd) --projectRoots $(pwd) --reset-cache &
 PACKAGER_PID=$!
 ./node_modules/.bin/react-native run-ios --no-packager
-./node_modules/.bin/dextrose run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
+sleep 20
+node "../bin/run" run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
 
-kill -9 $PACKAGER_PID
