ddc2e3b3d1f2f3b938d86e06bc1f97f7f55b77a4
L0wry <matthewlowry@hotmail.co.uk>
2018-05-01 15:22:55 +0100

    refactor: ios working

--- snapshots/run-ios.sh
+++ snapshots/run-ios.sh
@@ -3,6 +3,5 @@ xcrun simctl boot 'iPhone 7'
 rm snaps/*
 
 ./node_modules/.bin/react-native start --root $(pwd)  --reset-cache &
-
-# ./node_modules/.bin/react-native run-ios --no-packager
+./node_modules/.bin/react-native run-ios --no-packager
 node "../bin/run" run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
