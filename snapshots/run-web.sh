7f1f175c8cdf6f89af1135e2c86a0e9fcce23c79
L0wry <matthewlowry@hotmail.co.uk>
2018-05-02 13:16:54 +0100

    removing webpack log

--- snapshots/run-web.sh
+++ snapshots/run-web.sh
@@ -1,4 +1,4 @@
 ./node_modules/.bin/react-native start --reset-cache &
-./node_modules/.bin/fructose-web -d $(pwd)/fructose >> webpack.log &
+./node_modules/.bin/fructose-web -d $(pwd)/fructose &
 
 node "../bin/run" run --config ./fructose/dextrose.web.js -t 2000 --loglevel verbose
