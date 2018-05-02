948742689f2065dcf86b3e46390e66416ef38283
L0wry <matthewlowry@hotmail.co.uk>
2018-05-02 15:31:52 +0100

    reducing wait time

--- snapshots/run-ios.sh
+++ snapshots/run-ios.sh
@@ -4,4 +4,4 @@ rm snaps/*
 
 ./node_modules/.bin/react-native start --root $(pwd)  --reset-cache &
 ./node_modules/.bin/react-native run-ios --no-packager
-node "../bin/run" run --config ./fructose/dextrose.ios.js --snapshotWait 2000 --loglevel verbose
+node "../bin/run" run --config ./fructose/dextrose.ios.js --snapshotWait 500 --loglevel verbose
