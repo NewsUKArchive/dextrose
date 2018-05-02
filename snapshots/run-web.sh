39b300d7883df2e89d7c0049a32a0bb60416d21d
L0wry <matthewlowry@hotmail.co.uk>
2018-05-02 13:03:30 +0100

    fix: remove selenium

--- snapshots/run-web.sh
+++ snapshots/run-web.sh
@@ -1,5 +1,3 @@
-./node_modules/.bin/selenium-standalone install &&
-./node_modules/.bin/selenium-standalone start 2>> selenium.log & 
 ./node_modules/.bin/react-native start --reset-cache &
 ./node_modules/.bin/fructose-web -d $(pwd)/fructose >> webpack.log &
 
