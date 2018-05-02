22a72f7b5ba7a650c93dc61e72962ad4bee08985
L0wry <matthewlowry@hotmail.co.uk>
2018-05-02 13:49:31 +0100

    fix: adding index config

--- snapshots/index.js
+++ snapshots/index.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import fructose from '@times-components/fructose';
 import { loadStories } from './components';
 
-AppRegistry.registerComponent('snapshots', () => fructose(loadStories));
+AppRegistry.registerComponent('snapshots', () => fructose(loadStories, {platforn: 'native'}));
--- snapshots/index.web.js
+++ snapshots/index.web.js
@@ -3,7 +3,7 @@ import { AppRegistry } from 'react-native';
 import fructose from '@times-components/fructose';
 import { loadStories } from './components';
 
-AppRegistry.registerComponent('snapper', () => fructose(loadStories));
+AppRegistry.registerComponent('snapper', () => fructose(loadStories, { platform: "web" }));
 AppRegistry.runApplication('snapper', {
   rootTag: document.getElementById('react-root')
 });
