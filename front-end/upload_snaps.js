6571032ee1b54b52e297b47aee85f8ab2aa966ad
L0wry <matthewlowry@hotmail.co.uk>
2018-05-21 13:47:19 +0100

    fix: defensive upload

--- front-end/upload_snaps.js
+++ front-end/upload_snaps.js
@@ -14,38 +14,37 @@ module.exports = (bucket, commitHash, snapPath, opts) => {
     .readdirSync(snapPath)
     .map(f => `${path.join(process.cwd(), snapPath)}/${f}`);
 
-  if(files.length === 0) {
-    log.info('front-end', 'No snapshots found');
-    return;
+  if (!files.length === 0) {
+    const key = commitHash || 'no-commit-hash';
+
+    log.info('upload snapshots', `${files.length} images to be uploaded`);
+    log.info('upload snapshots', `Images to be sent to bucket key: ${key}`);
+
+    files.forEach((file) => {
+      const fileStream = fs.createReadStream(file);
+
+      fileStream.on('error', (err) => {
+        log.error('upload snapshots', err);
+      });
+
+      const uploadParams = {
+        Bucket: bucket,
+        Key: `${key}/${path.basename(file)}`,
+        Body: fileStream,
+        ContentType: 'image/png',
+      };
+
+      // call S3 to retrieve upload file to specified bucket
+      s3.putObject(uploadParams, (err, data) => {
+        if (err) {
+          log.error('upload snapshots', `${path.basename(file)} : ❌   Reason: ${err}`);
+        }
+        if (data) {
+          log.info('upload snapshots', `${path.basename(file)} : ✅`);
+        }
+      });
+    });
+  } else {
+    log.info('front-end', 'No snapshots found - skipping');
   }
-
-  const key = commitHash || 'no-commit-hash';
-
-  log.info('upload snapshots', `${files.length} images to be uploaded`);
-  log.info('upload snapshots', `Images to be sent to bucket key: ${key}`);
-
-  files.forEach((file) => {
-    const fileStream = fs.createReadStream(file);
-
-    fileStream.on('error', (err) => {
-      log.error('upload snapshots', err);
-    });
-
-    const uploadParams = {
-      Bucket: bucket,
-      Key: `${key}/${path.basename(file)}`,
-      Body: fileStream,
-      ContentType: 'image/png',
-    };
-
-    // call S3 to retrieve upload file to specified bucket
-    s3.putObject(uploadParams, (err, data) => {
-      if (err) {
-        log.error('upload snapshots', `${path.basename(file)} : ❌   Reason: ${err}`);
-      }
-      if (data) {
-        log.info('upload snapshots', `${path.basename(file)} : ✅`);
-      }
-    });
-  });
 };
